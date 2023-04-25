e9e45602887438f5c92c1c082a3875cc2b16d127
Mohammed Alshayah <90424258+Mohammed-Al-Shayah@users.noreply.github.com>
2023-04-25 12:44:21 +0300

    factory and seder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=> fake()->name(),
+            'content'=>fake()->paragraph(3,true),
+            'image'=>fake()->imageUrl(),
+            'salary'=>fake()->randomFloat(2,10,100),
+            'sale_price'=>fake()->optional()->randomFloat(2,5,50),
+            'quantity' => fake()->numberBetween(1, 100),
+        ];
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -14,11 +13,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
         DB::table('users')->insert([
             'name' => 'mohammedalshayah',
             'email' => 'mohammedtareq@gmail.com',
             'password' => Hash::make('123456789'),
             'type'=>'admin'
         ]);
+
+
     }
 }
